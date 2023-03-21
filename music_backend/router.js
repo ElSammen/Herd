9799869591953c8788431d753f1a11204511cac3
Liam <112492930+ElSammen@users.noreply.github.com>
2023-03-21 14:04:33 +0000

    token refresh fix

--- music_backend/router.js
+++ music_backend/router.js
@@ -47,8 +47,8 @@ router.post("/refresh", (req, res) => {
     .refreshAccessToken()
     .then(data => {
       res.json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
       })
     })
     .catch(err => {
