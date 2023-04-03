b56ab1919e6b2b908626eda0f980dd73b0d631b5
Liam <112492930+ElSammen@users.noreply.github.com>
2023-04-03 14:08:46 +0100

    quick update fix

--- music_backend/Controllers/UserController.js
+++ music_backend/Controllers/UserController.js
@@ -45,38 +45,39 @@ exports.create = async function (req, res) {
 };
 
 exports.update = async function (req, res) {
-//   console.log(req.body.genres.split(",").length);
-//   console.log("formData req:", req.body);
   try {
     //get length of the array in db
     const dbsize = await UserModel.findById(req.params.id);
-    // console.log(dbsize.genres.length);
+    console.log(dbsize.genres.length);
 
-    let user;
+    let user = dbsize;
+    const update = { ...req.body };
     //check if the array in db is less than 10, may differ if checked in postman
-    if (dbsize.genres.length <= 9) {
-      const update = { ...req.body };
-      console.log("update", update)
-      const genreArray = req.body.genres
-        .split(",")
-        .map((genre) => genre.trim());
-      {
+    if (update.genres) {
+      if (dbsize.genres.length <= 9) {
+        const genreArray = req.body.genres.split(',').map(genre => genre.trim());
         update.$push = { genres: { $each: genreArray } };
+        delete update.genres;
+      } else {
+        return res.send("Genre limit reached");
       }
-      delete update.genres; // remove genres property from the update object
-      user = await UserModel.findOneAndUpdate({ _id: req.params.id }, update,
-        {
-        runValidators: true,
-        new: true,
-      });
-    } else {
-      return res.send("Genre limit reached");
     }
-    console.log("user log:", user)
+
+    if (Object.keys(update).length === 0) {
+      // Nothing to update
+      return res.send(user);
+    }
+
+    user = await UserModel.findOneAndUpdate(
+      { _id: req.params.id },
+      update,
+      { runValidators: true, new: true }
+    );
+
     return res.send(user);
   } catch (error) {
-    if (error.name === "ValidationError") {
-      const errors = Object.values(error.errors).map((error) => error.message);
+    if (error.name === 'ValidationError') {
+      const errors = Object.values(error.errors).map(error => error.message);
       return res.status(400).send({ errors });
     } else {
       //TODO: other error handling
