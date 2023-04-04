ce6a15e89e8dc2b8e715a281b29b9240c8e11ede
Liam <112492930+ElSammen@users.noreply.github.com>
2023-04-04 15:57:44 +0100

    Updating URLS

--- music_frontend/src/Api/AuthApiCalls.jsx
+++ music_frontend/src/Api/AuthApiCalls.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 class AuthApiCalls extends AbstractApiClient {
     constructor() {
         super();
-        this.baseURL = "http://localhost:3001";
+        this.baseURL = "https://herd-backend.onrender.com/";
     }
 
     async loginOrRegister(data, operation) {
