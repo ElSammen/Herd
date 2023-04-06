6e48308b05bb40b815ad15d535df6fb068e9f107
Liam <112492930+ElSammen@users.noreply.github.com>
2023-04-06 12:01:05 +0100

    Update router.js

--- music_backend/router.js
+++ music_backend/router.js
@@ -25,7 +25,7 @@ router.get("/images/:genre", async (req, res) => {
     // console.log("hi")
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
-      redirectUri: "http://localhost:3000/home",
+      redirectUri: "https://herd-frontend.onrender.com/",
       clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     });
@@ -48,7 +48,7 @@ router.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   console.log("hi");
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000/home",
+    redirectUri: "https://herd-frontend.onrender.com/",
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     refreshToken,
