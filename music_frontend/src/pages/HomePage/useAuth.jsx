440a1dd77b2ac3f874d4774d252c6ce0e89b41cc
Liam <112492930+ElSammen@users.noreply.github.com>
2023-04-26 00:26:54 +0100

    Update useAuth.jsx

--- music_frontend/src/pages/HomePage/useAuth.jsx
+++ music_frontend/src/pages/HomePage/useAuth.jsx
@@ -9,7 +9,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("http://localhost:3001/login", {
+      .post("https://herd-backend.onrender.com/login", {
         code,
       })
       .then(res => {
@@ -27,7 +27,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-        .post("http://localhost:3001/refresh", {
+        .post("https://herd-backend.onrender.com/refresh", {
           refreshToken,
         })
         .then(res => {
